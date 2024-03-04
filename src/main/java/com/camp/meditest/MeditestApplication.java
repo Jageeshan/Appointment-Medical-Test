d82710720823d70c1676fb3bd8f9a7850c8b813e
Jageeshan <162009963+Jageeshan@users.noreply.github.com>
2024-03-04 11:16:12 +0530

    Add files via upload

---
+++ src/main/java/com/camp/meditest/MeditestApplication.java
@@ -0,0 +1,13 @@
+package com.camp.meditest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeditestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeditestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/camp/meditest/MeditestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camp.meditest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeditestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
